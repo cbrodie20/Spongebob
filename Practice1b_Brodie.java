32bdecbfa8de305149f91d2f0f8b1b640257ff1f
cbrodie20 <134465892+cbrodie20@users.noreply.github.com>
2023-05-23 22:51:39 -0400

    Add files via upload

---
+++ Practice1b_Brodie.java
@@ -0,0 +1,26 @@
+/*Caitlyn Brodie
+	8/29/2022
+	Period 5
+	This program will print out symbols to create a creature*/
+
+public class Practice1b_Brodie
+{
+	public static void main(String[] args)
+	{
+		System.out.println("\n\n\tSpongeBob\n");
+		System.out.println(" _____________________ ");
+		System.out.println("| o                o  |");
+		System.out.println("|       o             |");
+		System.out.println("|  ____        ____   |");
+		System.out.println("| |    |      |    |  |");
+		System.out.println("| | *  |      | *  |  |");
+		System.out.println("| |____|      |____|  |");
+		System.out.println("|                     |");
+		System.out.println("|  \\      )       /   |");
+		System.out.println("|   \\            /    |");
+		System.out.println("|    \\__________/   o |");
+		System.out.println("|  o               o  |");
+		System.out.println("|_____________________|");
+
+	}
+}
